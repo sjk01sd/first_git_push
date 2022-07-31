7019778458da865a75dcc4fb7403a9ca568d93e4
sjk01sd <sjk01ijk@gmail.com>
2022-07-31 20:32:08 +0530

    java program

---
+++ EvenorOdd.java
@@ -0,0 +1,22 @@
+
+ 
+
+class EvenorOdd {
+ 
+    
+    public static void main(String[] args)
+    {
+       
+        int num = 9;
+
+        if (num % 2 == 0) {
+ 
+            System.out.println("Entered Number is Even");
+        }
+ 
+        else {
+
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
